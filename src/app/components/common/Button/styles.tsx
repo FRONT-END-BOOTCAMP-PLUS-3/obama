8245d1fca47f399905e9c8cdd38ec1c29ef68732
전세계 <kssjssk@gmail.com>
2025-01-27 11:42:23 +0900

    Refactor /#3 import정리 하기 위한 index.ts 생성 유지보수성을 위한 코드 분리리

--- src/app/components/common/Button/styles.tsx
+++ src/app/components/common/Button/styles.tsx
@@ -1,14 +1,13 @@
-
-import styled,{ css } from "styled-components"
-
+import { css } from "styled-components";
 import type { ButtonVariant, ButtonSize } from "@/types/button"; 
-const sizeStyles: Record<ButtonSize, string> = {
+
+export const sizeStyles: Record<ButtonSize, string> = {
     s: "5.625rem",
     m: "8.75rem",
     l: "20rem",
 };
 
-const variantStyles: Record<ButtonVariant, ReturnType<typeof css>> = {
+export const variantStyles: Record<ButtonVariant, ReturnType<typeof css>> = {
     contained: css`
         background-color: var(--primary-color);
         color: var(--white-color);
@@ -53,31 +52,3 @@ const variantStyles: Record<ButtonVariant, ReturnType<typeof css>> = {
     }
     `,
 };
-
-/* Button styled component */
-const BaseButtonStyled = styled.button<{
-    variant: ButtonVariant; 
-    size: ButtonSize;
-}>`
-/* 공통 스타일 적용용 */
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    height: 2.5rem;
-    padding: 0.625rem 1.25rem;
-    font-weight: bold;
-    border-radius: 50%;
-    border: 1px solid var(--primary-color);
-    cursor: pointer;
-    transition: all 0.3s ease;
-
-    /* size에 따른 width 설정*/
-    ${({size}) => css`
-        width: ${sizeStyles[size]};
-    `}
-
-    /* variant에 따른 스타일 */
-    ${({variant}) => variantStyles[variant]}
-`
-
-export default BaseButtonStyled;
