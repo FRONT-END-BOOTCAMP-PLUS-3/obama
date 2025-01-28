783be2a3d703159e9b2de216be998ede7dfed5c3
goldJeong <manage06@naver.com>
2025-01-28 18:46:20 +0900

    Feat/#10 TextFiled 공통컴포넌트 구현
    - 텍스트필드(인풋창창) 공통컴포넌트 기능 구현
    - size, state에 따른 (default/current/error) 스타일 변경이 가능합니다.

--- src/app/components/common/Button/ButtonStyled.tsx
+++ src/app/components/common/Button/ButtonStyled.tsx
@@ -1,33 +1,31 @@
-
 import { ButtonSize, ButtonVariant } from "@/types/button";
-import styled,{ css } from "styled-components"
+import styled, { css } from "styled-components";
 import { sizeStyles, variantStyles } from "./styles";
-
 
 /* Button styled component */
 const BaseButtonStyled = styled.button<{
-    variant: ButtonVariant; 
-    size: ButtonSize;
+  variant: ButtonVariant;
+  size: ButtonSize;
 }>`
-/* 공통 스타일 적용용 */
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    height: 2.5rem;
-    padding: 0.625rem 1.25rem;
-    font-weight: bold;
-    border-radius: 50%;
-    border: 1px solid var(--primary-color);
-    cursor: pointer;
-    transition: all 0.3s ease;
+  /* 공통 스타일 적용용 */
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  height: 2.5rem;
+  padding: 0.625rem 1.25rem;
+  font-weight: bold;
+  border-radius: 50%;
+  border: 1px solid var(--primary-color);
+  cursor: pointer;
+  transition: all 0.3s ease;
 
-    /* size에 따른 width 설정*/
-    ${({size}) => css`
-        width: ${sizeStyles[size]};
-    `}
+  /* size에 따른 width 설정*/
+  ${({ size }) => css`
+    width: ${sizeStyles[size]};
+  `}
 
-    /* variant에 따른 스타일 */
-    ${({variant}) => variantStyles[variant]}
-`
+  /* variant에 따른 스타일 */
+    ${({ variant }) => variantStyles[variant]}
+`;
 
 export default BaseButtonStyled;
