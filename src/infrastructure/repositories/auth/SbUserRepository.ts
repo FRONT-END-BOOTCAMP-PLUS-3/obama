8a2023c68b2cae10f73913607bf1cf4efecdc0f5
전세계 <kssjssk@gmail.com>
2025-01-30 03:08:09 +0900

    Refactor/#9 클린아키텍처 구조로 인한 Repository refactoring

--- src/infrastructure/repositories/auth/SbUserRepository.ts
+++ src/infrastructure/repositories/auth/SbUserRepository.ts
@@ -1,13 +1,18 @@
 import { User } from '@/domain/entities/User';
 import { IUserRepository } from '@/domain/repositories/IUserRepository';
-import { createClient } from '@/utils/supabase/server';
+import { toSnakeCase } from '@/utils/convertCase';
+import { SupabaseClient } from '@supabase/supabase-js';
 
 export class SbUserRepository implements IUserRepository {
     private readonly tableName = 'user';
-    async createUser(user: User): Promise<void> {
-        const supabase = await createClient();
 
-        const {error} = await supabase.from(this.tableName).insert(user);
+    constructor(private readonly supabase: SupabaseClient) {}
+    
+    async createUser(user: User): Promise<void> {
+        
+        const userData = toSnakeCase(user);
+
+        const {error} = await this.supabase.from(this.tableName).insert(userData);
     
         if (error) {
             throw new Error(`Failed to create user: ${error.message}`);
