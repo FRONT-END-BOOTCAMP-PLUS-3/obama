82b4602e8a3af2f74a213826a534c810e22202b6
전세계 <kssjssk@gmail.com>
2025-02-03 21:34:23 +0900

    Refactor/#35 SRP를 하기 위한 duplicate 기능 제거

--- src/application/auth/usecases/SignUpUseCase.ts
+++ src/application/auth/usecases/SignUpUseCase.ts
@@ -1,22 +1,17 @@
 import { SignUpRequestDTO } from "@/application/auth/dtos/SignUpRequestDto";
 
-import { IDuplicateEmailUseCase } from "@/application/auth/interfaces/IDuplicateEmailUseCase";
 import { IUUIDGeneratorUseCase } from "@/application/common/usecases/interfaces/IUUIDGeneratorUseCase";
-import { IUserRepository } from "@/domain/repositories/IUserRepository";
 import { IPasswordHasher } from "@/application/common/usecases/interfaces/IPasswordHasherUseCase";
+import { IUserRepository } from "@/domain/repositories/IUserRepository";
+
 export class SignUpUseCase {
   constructor(
     private readonly userRepository: IUserRepository,
-    private readonly duplicateEmailUseCase: IDuplicateEmailUseCase,
     private readonly uuidGenerator: IUUIDGeneratorUseCase, // UUID 생성 유스케이스
     private readonly passwordHasher: IPasswordHasher
   ) {}
   async execute(request: SignUpRequestDTO): Promise<void> {
-    const isDuplicate = await this.duplicateEmailUseCase.execute(request.email);
-    if (isDuplicate) {
-      throw new Error("이미 사용 중인 이메일입니다.");
-    }
-
+    
     // UUID 생성
     const userId = this.uuidGenerator.generate();
 
