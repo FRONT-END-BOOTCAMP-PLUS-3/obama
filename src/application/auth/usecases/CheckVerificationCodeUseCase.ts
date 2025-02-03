02c62545ae0b8817c74b127fe54d12b282cbf73f
전세계 <kssjssk@gmail.com>
2025-02-04 01:18:09 +0900

    Refactor/#35 SRP를 위한 코드 분리

--- src/application/auth/usecases/CheckVerificationCodeUseCase.ts
+++ src/application/auth/usecases/CheckVerificationCodeUseCase.ts
@@ -1,11 +1,11 @@
 import { RdVerificationRepository } from "@/infrastructure/repositories/auth/RdVerificationRepository";
 
-export class VerifyCodeUseCase {
+export class CheckVerificationCodeUseCase {
     constructor(private verificationRepository : RdVerificationRepository) {}
 
     async execute(email: string, verificationCode: string): Promise<boolean> {
         const savedCode = await this.verificationRepository.getVerificationCode(email);
-
+        console.log("saved Redisusecases")
         if(!savedCode) {
             throw new Error("인증코드가 만료되었거나 존재하지 않습니다.");
         }
@@ -14,6 +14,7 @@ export class VerifyCodeUseCase {
 
         if(isValid) {
             await this.verificationRepository.deleteVerificationCode(email);
+            console.log("delete RedisUsecase");
         }
 
         return isValid;
