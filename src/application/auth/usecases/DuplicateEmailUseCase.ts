cace1fdcc17e2bd5f565ac34e9a042cfd7002b66
getsoss <getsoss@naver.com>
2025-02-05 11:09:41 +0900

    pull dev

--- src/application/auth/usecases/DuplicateEmailUseCase.ts
+++ src/application/auth/usecases/DuplicateEmailUseCase.ts
@@ -1,12 +1,9 @@
 import { IUserRepository } from "@/domain/repositories/IUserRepository";
-import { IDuplicateEmailUseCase } from "@/application/auth/interfaces/IDuplicateEmailUseCase";
-export class DuplicateEmailUseCase implements IDuplicateEmailUseCase  {
+export class DuplicateEmailUseCase {
+  constructor(private readonly userRepository: IUserRepository) {}
 
-    constructor(private readonly userRepository: IUserRepository) {}
-
-    async execute(email: string): Promise<boolean> {
-        const user = await this.userRepository.findByEmail(email);
-        return user !== null;
-    }
-
+  async execute(email: string): Promise<boolean> {
+    const user = await this.userRepository.findByEmail(email);
+    return user !== null;
+  }
 }
