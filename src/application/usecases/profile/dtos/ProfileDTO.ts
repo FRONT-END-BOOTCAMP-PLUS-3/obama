f72ee86d63765743cbbe1d921da5f972a4179b27
goldJeong <manage06@naver.com>
2025-02-09 21:57:59 +0900

    Refactor/#65 프로필 DTO에 기존 엔티티 재활용용

--- src/application/usecases/profile/dtos/ProfileDTO.ts
+++ src/application/usecases/profile/dtos/ProfileDTO.ts
@@ -1,23 +1,25 @@
-export interface ProfileDTO {
-    id: string;
-    picture: string;
-    name: string; 
-    birthDate: string; 
-    phone: string;
-    email: string;
+import { SNSInformation } from "@/domain/entities/profile/SNSInformation";
 
-    mbti: string;
-    hobbies: string[];
-    interests: string[];
-    trip: string[];
-    food: string[];
-    topic: string[]; 
-    worry: string;
-    lifeMovie: string;
-    ideal: string; 
-    recentSong: string;  
-    favoritePet: string; 
-    introduce: string;
-    snsInformation: { platform: string; url: string; id: string }[];
-  };
+export interface ProfileDTO {
+  id: string;
+  picture: string;
+  name: string;
+  birthDate: string;
+  phone: string;
+  email: string;
+
+  mbti: string;
+  hobbies: string[];
+  interests: string[];
+  trip: string[];
+  food: string[];
+  topic: string[];
+  worry: string;
+  lifeMovie: string;
+  ideal: string;
+  recentSong: string;
+  favoritePet: string;
+  introduce: string;
   
+  snsInformation: SNSInformation[];
+}
