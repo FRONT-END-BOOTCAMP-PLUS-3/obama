0d74d4e934b756262886d666154f52ca8c92757b
Hyein <snhyein2001@gmail.com>
2025-02-10 12:25:31 +0900

    Feat/#105 open question 질문 등록

--- src/app/api/admin/smalltalk/openquestion/route.ts
+++ src/app/api/admin/smalltalk/openquestion/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { SbOpenQuestionRepository } from "@/infrastructure/repositories/smalltalk/SbOpenQuestionRepository";
+import { CreateOpenQuestionUsecase } from "@/application/usecases/smalltalk/CreateOpenQuestionUsecase";
 import { OpenQuestionsUsecase } from "@/application/usecases/smalltalk/OpenQuestionUsecase";
 
 export async function GET(request: NextRequest) {
@@ -15,4 +16,25 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({ message: "Error fetching open questions" }, { status: 500 });
   }
 }
+
+export async function POST(request: NextRequest) {
+  try {
+    const requestData = await request.json(); 
+
+    const { subjectId, openQuestion } = requestData; 
+    if (!subjectId || !openQuestion) {
+      return NextResponse.json({ message: "Missing required fields" }, { status: 400 });
+    }
+
+    const repository = new SbOpenQuestionRepository();
+    const usecase = new CreateOpenQuestionUsecase(repository);
+    const updatedQuestions = await usecase.execute(subjectId, openQuestion);
+
+    return NextResponse.json({ questions: updatedQuestions }, { status: 200 });
+  } catch (error) {
+    console.error("Error adding question:", error);
+    return NextResponse.json({ message: "Error adding question" }, { status: 500 });
+  }
+}
+
 
