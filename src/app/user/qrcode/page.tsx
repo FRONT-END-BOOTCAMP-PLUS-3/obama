f6a5078ec2d77c5360142622d3e5210e1f330a29
getsoss <getsoss@naver.com>
2025-02-10 15:35:27 +0900

    Feat/#106 수파베이스 이미지 조회

--- src/app/user/qrcode/page.tsx
+++ src/app/user/qrcode/page.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>page</div>;
+import React, { useState, useEffect } from "react";
+import {
+  QrcodeCreateContainer,
+  QrTitle,
+  QrTitleSection,
+} from "@/components/qrcode/QrcodePage.Styeld";
+import * as S from "@/components/items/ProfileUploader.Styled";
+import { createClient } from "@supabase/supabase-js";
+import { clientConfig } from "@/config/clientEnv";
+
+const QrCreatePage: React.FC = () => {
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const supabase = createClient(
+    clientConfig.NEXT_PUBLIC_SUPABASE_URL,
+    clientConfig.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+  useEffect(() => {
+    const getImageUrl = async () => {
+      const { data } = supabase.storage
+        .from("profile-images")
+        .getPublicUrl("profiles/1739164740550.jpeg");
+
+      if (data) {
+        setImageUrl(data.publicUrl);
+      }
+    };
+
+    getImageUrl();
+  }, []);
+
+  return (
+    <QrcodeCreateContainer>
+      <S.ImageWrapper>
+        <S.ProfileImage
+          src={imageUrl || "/icons/profilePicture.svg"}
+          alt="Profile"
+        />
+      </S.ImageWrapper>
+      <QrTitleSection>
+        <QrTitle>QR코드를 생성해주세요.</QrTitle>
+      </QrTitleSection>
+    </QrcodeCreateContainer>
+  );
 };
 
-export default page;
+export default QrCreatePage;
---
+++ src/components/qrcode/QrcodePage.Styeld.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import styled from "styled-components";
+
+export const QrcodeCreateContainer = styled.div`
+  margin-top: 5rem;
+  padding: 2rem; /* 32px -> 2rem */
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+  justify-content: space-between;
+`;
+
+export const QrTitle = styled.h4`
+  margin-bottom: 6.56rem;
+  text-align: center;
+`;
+
+export const QrTitleSection = styled.div`
+  display: flex;
+  justify-content: center;
+`;
