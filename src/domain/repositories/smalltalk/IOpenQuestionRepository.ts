d131c6a48fc199a99b2e298ee9cc7f39b0eacfb1
Hyein <snhyein2001@gmail.com>
2025-02-10 10:51:22 +0900

    Feat/#103 admin open question db 연결

--- src/domain/repositories/smalltalk/IOpenQuestionRepository.ts
+++ src/domain/repositories/smalltalk/IOpenQuestionRepository.ts
@@ -2,4 +2,5 @@ import { OpenQuestion } from "@/domain/entities/smalltalk/OpenQuestion";
 
 export interface IOpenQuestionRepository {
   findBySubjectId(subjectId: number): Promise<OpenQuestion[]>;
+  findAll(): Promise<OpenQuestion[]>; 
 }
