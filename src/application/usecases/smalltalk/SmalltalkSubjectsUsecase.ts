7bc71300d4f58292be5172a766ceffb4fc9c0474
Hyein <snhyein2001@gmail.com>
2025-02-11 17:12:41 +0900

    Feat/#125 balancegame update error fix

--- src/application/usecases/smalltalk/SmalltalkSubjectsUsecase.ts
+++ src/application/usecases/smalltalk/SmalltalkSubjectsUsecase.ts
@@ -5,7 +5,8 @@ export class GetSmalltalkSubjectsUsecase {
   constructor(private readonly repository: IsmalltalkSubjectRepository) {}
 
   async execute(): Promise<SmalltalkSubjectDto[]> {
-    return await this.repository.findAll(); 
+    const subjects = await this.repository.findAll();
+    return subjects ?? [];
   }
 }
 
