756432fde64881a26cbbe3b5f0902d31f005f447
전세계 <kssjssk@gmail.com>
2025-02-11 15:26:03 +0900

    Style/회원탈퇴 시 page이동

--- src/components/dashboard/AccountSection.tsx
+++ src/components/dashboard/AccountSection.tsx
@@ -1,8 +1,11 @@
 import TextButton from "@/components/common/button/TextButton";
 import { AccountButtonLayer, AccountWrapper, Title } from "@/components/dashboard/DashBoard.Styled";
 import { logout } from "@/utils/auth/logout";
+import { useRouter } from "next/navigation";
 
 const AccountSection = () => {
+
+  const router = useRouter()
 
   const handleClickLogout =() => {
 
@@ -11,6 +14,8 @@ const AccountSection = () => {
   }
 
   const handleClickAccountDelete= () => {
+    router.push("/user/withdraw")
+
     // api 요청
 
   }
