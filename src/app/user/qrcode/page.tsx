44d3acc3b9581762b3fae6b8477c2140a0334217
getsoss <getsoss@naver.com>
2025-02-12 12:14:18 +0900

    Feat/#106 본인 uuid로 라우팅 되는 QR 코드 생성

--- src/app/user/qrcode/page.tsx
+++ src/app/user/qrcode/page.tsx
@@ -15,17 +15,22 @@ import { useQRCode } from "next-qrcode";
 const QrCreatePage: React.FC = () => {
   const { Canvas } = useQRCode();
   const [imageUrl, setImageUrl] = useState<string>("");
+  const [qrUrl, setQrUrl] = useState<string | null>(null);
   const supabase = createClient(
     clientConfig.NEXT_PUBLIC_SUPABASE_URL,
     clientConfig.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
 
   useEffect(() => {
-    const getImageUrl = async () => {
+    const getUserData = async () => {
       const userId = localStorage.getItem("userId");
-      if (!userId) return; // userId가 없으면 실행하지 않음
+      if (!userId) return;
 
-      const imagePath = `profiles/${userId}.png`; // 동적으로 경로 생성
+      // QR 코드 URL 동적으로 설정
+      setQrUrl(`${window.location.origin}/users/${userId}`);
+
+      // 프로필 이미지 가져오기
+      const imagePath = `profiles/${userId}.png`;
       const { data } = supabase.storage
         .from("profile-images")
         .getPublicUrl(imagePath);
@@ -35,7 +40,7 @@ const QrCreatePage: React.FC = () => {
       }
     };
 
-    getImageUrl();
+    getUserData();
   }, []);
 
   return (
@@ -50,19 +55,21 @@ const QrCreatePage: React.FC = () => {
         <QrTitle>QR코드를 스캔해주세요.</QrTitle>
       </QrTitleSection>
       <QrcodeSection>
-        <Canvas
-          text={"https://github.com/bunlong/next-qrcode"}
-          options={{
-            errorCorrectionLevel: "M",
-            margin: 3,
-            scale: 4,
-            width: 200,
-            color: {
-              dark: "#000000",
-              light: "#ffffff",
-            },
-          }}
-        />
+        {qrUrl && (
+          <Canvas
+            text={qrUrl}
+            options={{
+              errorCorrectionLevel: "M",
+              margin: 3,
+              scale: 4,
+              width: 200,
+              color: {
+                dark: "#000000",
+                light: "#ffffff",
+              },
+            }}
+          />
+        )}
       </QrcodeSection>
     </QrcodeCreateContainer>
   );
